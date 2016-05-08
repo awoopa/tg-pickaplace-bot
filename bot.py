ecc10f0cba94c36aded49e4ac0083b3ed6bd6376
William Qi <hello@williamqi.com>
2016-05-07 22:24:17 -0700

    update library calls

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
-from telegram import Updater
+from telegram.ext import Updater
+from telegram.ext import CommandHandler
 
-TELEGRAM_TOKEN = "INSERT_YOUR_TOKEN_HERE"
+TELEGRAM_TOKEN = "130757743:AAGbbBfXDNGnmpYqvbfC4-mXlDcD1V4i2tQ"
 
 updater = Updater(token=TELEGRAM_TOKEN)
 dispatcher = updater.dispatcher
@@ -11,6 +12,6 @@ def echo(bot, update):
 
   bot.sendMessage(chat_id=chat_id, text=message_text)
 
-
-dispatcher.addTelegramMessageHandler(echo)
-updater.start_polling
+echo_handler = CommandHandler('', echo)
+dispatcher.addHandler(echo_handler)
+updater.start_polling()
